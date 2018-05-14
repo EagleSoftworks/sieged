5d977b3bff1859071ff41482d6b31e187528689f
xSpacklesx <hunterspack@gmail.com>
2018-05-14 11:26:06 -0400

    add core classes

---
+++ Assets/Unit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    public int hp;
+    public int damage;
+    public int popCost;
+    public int goldCost;
+    public string level;
+    public string type;
+
+    public Unit(string type, string level, int goldCost, int popCost, int hp, int damage)
+    {
+        this.type = type;
+        this.level = level;
+        this.goldCost = goldCost;
+        this.popCost = popCost;
+        this.hp = hp;
+        this.damage = damage;
+    }
+ 
+}
+

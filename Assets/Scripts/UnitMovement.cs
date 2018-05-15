bf63565bc5a4f9c50d897a176f74045e1898e1f0
d_richardi <d_richardi@sau9.org>
2018-05-15 10:56:10 -0400

    split unitmanagement.cs added unit prefab

---
+++ Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour {
+
+    public Transform target;
+    private float speed = 5;
+    /*
+            // one time at object init
+            void Start () {
+                //target = FindObjectOfType<PlayerControl>().transform; // this appears to be a metaprogramming hack
+            }
+
+    */
+    // every frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        Debug.Log(target.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}

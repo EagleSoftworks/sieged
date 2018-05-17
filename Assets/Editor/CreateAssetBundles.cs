30ea044397d1a2d011a6ad3171dc3a6357d8aa22
d_richardi <d_richardi@sau9.org>
2018-05-17 10:48:13 -0400

    Asset Bundle LZ4/Chunkbased compression for WebGL build

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -11,6 +11,6 @@ public class CreateAssetBundles
         {
             Directory.CreateDirectory(assetBundleDirectory);
         }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.WebGL);
     }
 }

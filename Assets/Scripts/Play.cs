abacc4ed96e0f464231fc013493878eec56b0916
esmit2000 <esmit10272000@gmail.com>
2018-06-06 10:16:42 -0400

    hopefully select UI implemented

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Play : MonoBehaviour {
+
+    public Button play;
+    public string next;
+    public GameObject isNull;
+    public GameObject isActive;
+
+	// Use this for initialization
+	void Start () {
+        play.onClick.AddListener(move);
+	}
+
+    void move () {
+        isNull.SetActive(false);
+        isActive.SetActive(true);
+        SceneManager.LoadScene(next);
+    }
+}
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Transition : MonoBehaviour {
+
+    public GameObject saved;
+    public InputField userInput;
+    public Text playerName;
+
+	// Use this for initialization
+	void Start () {
+        playerName.text = userInput.text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerPrefs.SetString("Name", userInput.text); 
+        playerName.text = PlayerPrefs.GetString("Name");
+        DontDestroyOnLoad(saved);
+	}
+}
